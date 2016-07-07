9ad6ba30d52250f072374e0af91da5977ba288b5
jian.elegant <jian.elegant@gmail.com>
2016-07-07 10:02:57 +0800

    runtime permission check

--- app/src/main/java/com/cos/mos/sr/MainActivity.java
+++ app/src/main/java/com/cos/mos/sr/MainActivity.java
@@ -1,13 +1,23 @@
 package com.cos.mos.sr;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.cos.mos.sr.service.SRService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String[] PERMISSIONS_NEED_REQUEST = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private static final int PERMISSION_REQUEST_CODE = 1989;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,33 @@ public class MainActivity extends AppCompatActivity {
                 startService(new Intent(MainActivity.this, SRService.class));
             }
         });
+
+        checkPermissions();
+    }
+
+    private void checkPermissions() {
+        List<String> pers = new ArrayList<>();
+        for(String per : PERMISSIONS_NEED_REQUEST){
+            if(ActivityCompat.checkSelfPermission(MainActivity.this, per) == PackageManager.PERMISSION_DENIED){
+                pers.add(per);
+            }
+        }
+        int size = pers.size();
+        if(size > 0){
+            ActivityCompat.requestPermissions(MainActivity.this, pers.toArray(new String[size]), PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(PERMISSION_REQUEST_CODE == requestCode){
+            int length = grantResults.length;
+            for(int i = 0 ; i < length;i++){
+                if(PackageManager.PERMISSION_DENIED == grantResults[i]){
+                    // TODO permission denied.
+                }
+            }
+        }
     }
 }
